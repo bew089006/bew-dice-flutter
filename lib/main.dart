eacf28819d6eabbc8dee00c61dd8a29513bda604
wattanan <bew08900@gmail.com>
2020-12-05 16:43:11 +0700

    completed dice random app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee2'),
+          title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +16,55 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int LeftDiceNumber = 1;
+  int RightDiceNumber = 1;
+
+  randomDice() {
+    setState(() {
+      LeftDiceNumber = Random().nextInt(6) + 1;
+      RightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  resetRandomDice() {
+    LeftDiceNumber = 1;
+    RightDiceNumber = 1;
+  }
+
+  Dice(sideDice) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            randomDice();
+          });
+          print('Left Button is clicked');
+        },
+        onLongPress: () {
+          setState(() {
+            resetRandomDice();
+          });
+        },
+        child: Image.asset('images/dice$sideDice.png'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Dice(LeftDiceNumber),
+          Dice(RightDiceNumber),
+        ],
+      ),
+    );
   }
 }
